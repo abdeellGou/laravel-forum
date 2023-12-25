e0d78f6477d348dc2c856282a7aa58dc13171722
abdeell <goumhaned.abdallah@gmail.com>
2023-12-25 11:34:26 +0100

    Generation Relations between models

---
+++ app/Models/Question.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Str;
+
+class Question extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = ['title','body'];
+
+    //set relation between Model Question And User
+    public function user() : BelongsTo
+    { 
+        return $this->belongsTo(
+            \App\Models\User::class,
+            'by_user_id'
+        );
+    } 
+
+    public function setTitleAttribute($value)
+    {
+        $this->attributes['title'] = $value;
+        $this->attributes['slug'] = Str::slug($value);
+    }
+}
